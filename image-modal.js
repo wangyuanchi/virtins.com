e86ff20b000ff331736f7a770766e77858cb34dd
wangyuanchi <yuanchiri@gmail.com>
2024-03-19 19:51:26 +0800

    Minor Text and Image Adjustments

--- image-modal.js
+++ image-modal.js
@@ -1,17 +1,33 @@
 const modal = document.querySelector(".modal");
 const figureList = document.querySelectorAll(".figure");
+const anchorList = document.querySelectorAll(".modal__anchor");
 
 const modal__content = document.querySelector(".modal__content");
 const modal__caption = document.querySelector(".modal__caption");
 
+// for figures
 figureList.forEach((figure) => {
   let image = figure.querySelector("img");
   let caption = figure.querySelector("figcaption");
   image.style.cursor = "pointer";
   image.addEventListener("click", () => {
     modal.style.display = "block";
-    modal__content.src = image.src.replace("-m.png", ".png");
+    modal__content.src = image.src.replace("-m.png", ".png"); // some pictures do not have -m
     modal__caption.innerText = caption.innerText;
+  });
+});
+
+// for anchors and its corresponding image
+anchorList.forEach((anchor) => {
+  anchor.style.cursor = "pointer";
+  let modalID = anchor.id.replace("anchor", "image");
+  let targetModal = document.getElementById(modalID);
+  let targetModalClose = targetModal.querySelector(".modal__close");
+  anchor.addEventListener("click", () => {
+    targetModal.style.display = "block";
+  });
+  targetModalClose.addEventListener("click", () => {
+    targetModal.style.display = "none";
   });
 });
 
